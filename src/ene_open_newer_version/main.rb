36d1b3d3121b72151a061317bf277fd7b6d02f08
Eneroth3 <eneroth3@gmail.com>
2018-06-20 21:06:56 +0200

    Show directly below "Open..." in menu
    In SU 2016 and later.

--- src/ene_open_newer_version/main.rb
+++ src/ene_open_newer_version/main.rb
@@ -36,6 +36,39 @@ module OpenNewerVersion
     nil
   end
 
+  # Add menu item at custom position in menu.
+  #
+  # The custom position is only used in SketchUp for Windows version 2016 and
+  # above. In other versions the menu item will be placed at the end of the
+  # menu. Please note that this is an undocumented SketchUp API behavior that
+  # may be subject to change.
+  #
+  # @param menu [UI::Menu]
+  # @param name [String]
+  # @param position [Fixnum, nil]
+  #
+  # @example
+  #   # Add Menu Item Right Below Entity Info
+  #   UI.add_context_menu_handler do |menu|
+  #     SkippyLib::LUI.add_menu_item(menu, "Entity Color Info", 1) do
+  #       model = Sketchup.active_model
+  #       entity = model.selection.first
+  #       return unless entity
+  #       color = entity.material ? entity.material.color : model.rendering_options["ForegroundColor"]
+  #       UI.messagebox(color.to_a.join(", "))
+  #     end
+  #   end
+  #
+  # @return [Fixnum] identifier of menu item.
+  def self.add_menu_item(menu, name, position = nil, &block)
+    # Sketchup.platform added in SU2014.
+    if position && Sketchup.platform == :platform_win && Sketchup.version.to_i >= 16
+      menu.add_item(name, position, &block)
+    else
+      menu.add_item(name, &block)
+    end
+  end
+
   # Reload extension.
   #
   # @param clear_console [Boolean] Whether console should be cleared.
@@ -63,9 +96,7 @@ module OpenNewerVersion
     @loaded = true
 
     menu = UI.menu("File")
-    cmd = UI::Command.new("Open Newer Version") { open_newer_version }
-    # TODO: Add directly below "Open".
-    menu.add_item(cmd)
+    add_menu_item(menu, "Open Newer Version...", 2) { open_newer_version }
   end
 
 end
