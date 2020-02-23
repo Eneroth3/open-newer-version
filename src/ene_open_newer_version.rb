83a1250f96e999e64de447d0081725e969b19a96
Eneroth3 <eneroth3@gmail.com>
2020-02-23 11:18:10 +0100

    Bump version number

--- src/ene_open_newer_version.rb
+++ src/ene_open_newer_version.rb
@@ -27,8 +27,8 @@ module OpenNewerVersion
   EXTENSION.creator     = "Julia Christina Eneroth"
   EXTENSION.description =
     "Convert and open models made in newer versions of SketchUp."
-  EXTENSION.version     = "1.0.4"
-  EXTENSION.copyright   = "2019, #{EXTENSION.creator}"
+  EXTENSION.version     = "1.0.5"
+  EXTENSION.copyright   = "2020, #{EXTENSION.creator}"
   Sketchup.register_extension(EXTENSION, true)
 
 end
