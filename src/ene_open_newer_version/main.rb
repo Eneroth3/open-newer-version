018e00e5747840fe49548b33f7a16458a2b93915
Eneroth3 <eneroth3@gmail.com>
2020-02-27 14:01:47 +0100

    Correct method name spelling

--- src/ene_open_newer_version/main.rb
+++ src/ene_open_newer_version/main.rb
@@ -1,12 +1,12 @@
 module Eneroth
   module OpenNewerVersion
     if !Sketchup.platform == :platform_win
-      UI.messahebox("#{EXTENSION.name} is only supported on Windows.")
+      UI.messagebox("#{EXTENSION.name} is only supported on Windows.")
     elsif Sketchup.respond_to?(:is_64bit?) && !Sketchup.is_64bit?
       # Can't detect and handle 32 bitness in SU 2014 :( .
-      UI.messahebox("#{EXTENSION.name} requires 64 bit Windows.")
+      UI.messagebox("#{EXTENSION.name} requires 64 bit Windows.")
     elsif Sketchup.version.to_i < 14
-      UI.messahebox("#{EXTENSION.name} requires SketchUp 2014 or newer.")
+      UI.messagebox("#{EXTENSION.name} requires SketchUp 2014 or newer.")
     else
       Sketchup.require File.join(PLUGIN_DIR, "menu")
     end
